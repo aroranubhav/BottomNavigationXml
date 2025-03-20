2bfcc65966c84f24e5ee0db8c3aca5b6dc728fd9
maxi <arora.anubhav93@gmail.com>
2025-03-20 16:03:49 +0530

    Add logic to handle bottom nav clicks
    This commit handles bottom navigation view clicks to launch respective fragments

--- app/src/main/java/com/almax/bottomnavigationxml/MainActivity.kt
+++ app/src/main/java/com/almax/bottomnavigationxml/MainActivity.kt
@@ -1,14 +1,54 @@
 package com.almax.bottomnavigationxml
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
+import androidx.fragment.app.Fragment
+import com.almax.bottomnavigationxml.databinding.ActivityMainBinding
+import com.almax.bottomnavigationxml.first.FirstFragment
+import com.almax.bottomnavigationxml.second.SecondFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupUi()
+    }
+
+    private fun setupUi() {
+        val bottomNavigationView = binding.navView
+        val firstFragment = FirstFragment.newInstance()
+        val secondFragment = SecondFragment.newInstance()
+
+        bottomNavigationView.setOnApplyWindowInsetsListener { view, insets ->
+            view.updatePadding(0, 0, 0, 0)
+            insets
+        }
+
+        setCurrentFragment(firstFragment)
+
+        bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.navFirst -> {
+                    setCurrentFragment(firstFragment)
+                }
+
+                R.id.navSecond -> {
+                    setCurrentFragment(secondFragment)
+                }
+            }
+            true
+        }
+    }
+
+    private fun setCurrentFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragmentContainer, fragment)
+                .commit()
+        }
     }
 }
